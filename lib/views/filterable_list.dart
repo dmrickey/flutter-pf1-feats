c5170fe6fea02e11f84b1a2d26cccb114930cdfd
dmrickey <dmrickey@gmail.com>
2022-07-11 16:10:47 -0500

    Fixed android

--- lib/views/filterable_list.dart
+++ lib/views/filterable_list.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:sembast/sembast.dart';
-import 'package:sembast/sembast_io.dart';
+import 'package:flutter/services.dart';
 
 import '../models/feat.dart';
 import 'description_view.dart';
@@ -88,25 +89,7 @@ class _FeatsListState extends State<FeatsList> {
   }
 
   void initStateAsync() async {
-    /* list all files in database dir */
-    // final dir = Directory('./assets/database');
-    // final List<FileSystemEntity> entities = await dir.list().toList();
-    // entities.forEach(print);
-
-    /* uncomment this and method below to re-import feats db */
-    // await importFeats();
-
-    /* make sure this file is defined in pubspec.yaml assets*/
-    String dbPath = 'assets/database/feats.db';
-    DatabaseFactory dbFactory = databaseFactoryIo;
-    Database db = await dbFactory.openDatabase(dbPath);
-
-    var store = intMapStoreFactory.store('feats');
-
-    final recordSnapshot = await store.find(db);
-    // should be mapping `snapshot.value` to a real object, but I'm lazy and haven't done that yet
-    var loaded =
-        recordSnapshot.map((snapshot) => Feat(snapshot.value)).toList();
+    var loaded = await loadFeats();
     loaded.sort((a, b) => a.name.compareTo(b.name));
     setState(() {
       _loaded = loaded;
@@ -128,19 +111,8 @@ class _FeatsListState extends State<FeatsList> {
     super.setState(() {});
   }
 
-  // @override
-  // void setState(fn) {
-  //   super.setState(fn);
-  // }
-
-  /* import feats from previously exported file */
-  // Future importFeats() async {
-  /* make sure this file is defined in pubspec.yaml assets*/
-  //   var featsJson =
-  //       await rootBundle.loadString('assets/database/feats.export.json');
-  //   var map = jsonDecode(featsJson) as Map;
-  //   DatabaseFactory dbFactory = databaseFactoryIo;
-  //   var importedFeatsDb =
-  //       await importDatabase(map, dbFactory, 'assets/database/feats.db');
-  // }
+  Future<List<Feat>> loadFeats() async {
+    var featsString = await rootBundle.loadString('assets/database/feats.json');
+    return json.decode(featsString).map<Feat>((f) => Feat(f)).toList();
+  }
 }
